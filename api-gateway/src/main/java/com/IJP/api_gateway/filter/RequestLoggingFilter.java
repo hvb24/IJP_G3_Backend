03b8983b933982b63f5b404adcd16c321c4a8d3c
vishal.goswami <vishalgoswami02018@gmail.com>
2024-08-19 12:09:29 +0530

    pushed entire code except authentication filter

---
+++ api-gateway/src/main/java/com/IJP/api_gateway/filter/RequestLoggingFilter.java
@@ -0,0 +1,27 @@
+package com.IJP.api_gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+public class RequestLoggingFilter implements GlobalFilter, Ordered {
+    private static final Logger logger = LoggerFactory.getLogger(RequestLoggingFilter.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        logger.info("Request Method: {}, Request URI: {}", request.getMethod(), request.getURI());
+        return chain.filter(exchange);
+    }
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
